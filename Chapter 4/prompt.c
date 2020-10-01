9da1f03fa4f6ff71dbc6975c978083034c297a51
Anis <59984748+anistarafdar@users.noreply.github.com>
2020-10-01 07:31:21 -0400

    im beggining to be doubtful of how good this book is

--- Chapter 4/prompt.c
+++ Chapter 4/prompt.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// declare a buffer for user input of size 2048
-static char input[2048];
+#include <readline/history.h>
+#include <readline/readline.h>
 
 // "char** argv" and "char **argv" both mean the same thing, a pointer to a character pointer
 int main(int argc, char** argv) { 
 
 	// print version and how to exit
-
 	puts("Lisp Interpreter for C, v0.0.0.0.1");
 	puts("Press Ctrl+C to Exit\n");
-
 
 	// loop will never end
 	while (1){
 	
-		// output prompt
-		fputs("lisp> ", stdout);
+		// output prompt and then read input
+		char *input = readline("lisp> ");
 
-		// read	user input, maximum size 2048, i wonder what happens if 2049 chars are inputted
-		fgets(input, 2048, stdin);
-		// my guess is that fgets puts the null termination byte for me at the end of my array
-		// and porbably taking care of a chance of overrun for me
+		// add input to history
+		add_history(input);		
 
-		// simply repeats it back the input for now
-		printf("Oh well, %s", input);				
+		// simply echos back input 
+		printf("Oh well, %s to you too!\n", input);
+
+		// free retrieved input
+		free(input);
 	}
 
 	return EXIT_SUCCESS;
 }
+// what are these new editline headers and commands doing...
