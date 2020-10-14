9f039d74bcebc10d3ad080980b1e4e5666108613
Anis <59984748+anistarafdar@users.noreply.github.com>
2020-10-13 22:09:53 -0400

    Add files via upload
    comfortable is the working one lol

--- Chapter 10/q_expression_answer.c
+++ Chapter 10/q_expression_answer.c
@@ -16,8 +16,8 @@ char* readline(char* prompt) {
 void add_history(char* unused) {}
 
 #else
-#include <editline/readline.h>
-#include <editline/history.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #endif
 
 /* Add QEXPR as possible lval type */
